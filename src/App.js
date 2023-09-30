481afa93326248a16954458a628b537b56b512b5
Dev-Corgi <bora5242@gmail.com>
2023-09-30 13:44:18 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+//목표: 패널의 확인 버튼을 누르면 패널이 사라지고 카운터가 나타나야 합니다,
+// 카운터의 증가 버튼을 누르면 카운터가 증가하고, 카운터의 감소버튼을 누르면 카운터가 감소해야 합니다.ㄴ
 
+import "./App.css";
+import React, { useState } from "react";
+import Panel from "./Components/Panel/Panel";
+import Counter from "./Components/Counter/Counter";
 function App() {
+  //useState를 사용하여 컨디셔널 랜딩에 사용할 isCounter 변수와
+  //isCounter를 세팅할 setisCounter를 정의하세요
+
+  // "" 자리에 컴포넌트 코드를 참고하여, 컨디셔널 랜딩이 이루어 지도록 적절한 값을 대입하세요
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Counter isCounter ={""}></Counter>
+        <Panel isCounter={""} setisCounter = {""}></Panel>
     </div>
   );
 }
